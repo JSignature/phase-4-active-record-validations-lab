c8ced1fbcbd8d25abbbf58ac2b8d87aa556555f5
Jimmy Andreas <jimmysignature@gmail.com>
2022-08-26 21:06:43 -0400

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :click_bate
+    def click_bate
+
+
+    if :title != "Won't Believe" || "Secret" || "Top [number]" || "Guess"
+      errors.add(:click_bate, "can't be greater than total value")
+    
+
+    end
+
+
+
 end
